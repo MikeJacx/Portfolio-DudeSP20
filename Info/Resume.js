6047e786460f8ec69681d8eeca5752a0413fe704
MikeJacx <60370805+MikeJacx@users.noreply.github.com>
2020-05-11 15:19:25 -0500

    Last commit

---
+++ Info/Resume.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  
+    $(".modal-link").click(function(){
+      $(".modal").toggle();
+    });
+    
+    $(".modal-background").click(function(){
+        $(".modal").toggle();
+      })
+
+      $(".modal-close").click(function(){
+        $(".modal").toggle();
+      })
+    })
